83ac642c44469d42f018ccc0421c83cc55252720
dainylcua <dainylcua@gmail.com>
2022-01-07 12:40:51 -0600

    Nav link styling

--- components/Nav.js
+++ components/Nav.js
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function Nav() {
   return(
     <nav className="h-auto">
-      <div className="flex justify-between px-12 py-4">
+      <div className="flex justify-between py-4">
         <div className="flex justify-between w-3/5">
           <NavLink name="Home" href=""/>
           <NavLink name="Blogposts" href="blogposts"/>
@@ -14,8 +14,8 @@ export default function Nav() {
           <NavLink name="Pomodoro" href="pomodoro"/>
           <NavLink name="My Info" href="my-info"/>
         </div>
-        <div className="self-end w-1/5">
-            <IconContext.Provider value={{ className: "text-zinc-900 dark:text-rose-200 cursor-pointer", size: "1.5em"}}>
+        <div className="w-1/5 self-center pr-4">
+            <IconContext.Provider value={{ className: "text-zinc-900 hover:text-zinc-500 dark:text-rose-200 dark:hover:text-rose-100 cursor-pointer ", size: "1.5em"}}>
               <div className="flex justify-between">
                 <Link passHref href="https://github.com/dainylcua/">
                   <SiGithub />
--- components/NavLink.js
+++ components/NavLink.js
@@ -1,8 +1,11 @@
-import Link from "next/link";
+import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function NavLink({name, href}) {
+  const router = useRouter()
+  const isActive = router.asPath === `/${href}` ? 'dark:text-rose-200' : 'text-zinc-500'
   return(
-    <div>
+    <div className={`dark:hover:bg-zinc-700 hover:bg-zinc-300 p-1.5 ${isActive} rounded-md transition-colors ease-in-out`}>
       <Link passHref href={`/${href}`}>
         <a>{name}</a>
       </Link>
