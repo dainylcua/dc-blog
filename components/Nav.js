bc546260fda0ba87c1728cd2e63d472717d34e7a
dainylcua <dainylcua@gmail.com>
2022-01-07 12:01:19 -0600

    Adds icons to Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,28 @@
+import { SiGithub, SiLinkedin, SiTwitter  } from "react-icons/si"
+import { IconContext } from "react-icons/lib"
+
 export default function Nav() {
   return(
-    <nav className="border-b-zinc-100">
-      <div>nav</div>
+    <nav className="h-auto">
+      <div className="flex justify-between px-12 py-4">
+        <div className="flex justify-between w-3/5">
+          <div>Home</div>
+          <div>Blogposts</div>
+          <div>Goals</div>
+          <div>Timer</div>
+          <div>My Info</div>
+        </div>
+        <div className="self-end w-1/5">
+            <IconContext.Provider value={{ className: "text-zinc-900 dark:text-rose-200", size: "1.5em"}}>
+              <div className="flex justify-between">
+                <SiGithub />
+                <SiLinkedin />
+                <SiTwitter />
+              </div>
+            </IconContext.Provider>
+        </div>
+      </div>
+      <div className="border border-zinc-900 dark:border-rose-200 rounded-b-full h-[1.5px]"></div>
     </nav>
   )
 }
