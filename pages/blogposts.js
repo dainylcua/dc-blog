06ef43a869a2fe5d713c7f3dac1067d18989c746
dainylcua <dainylcua@gmail.com>
2022-01-08 11:36:21 -0600

    Blogpost links query works

--- pages/blogposts.js
+++ pages/blogposts.js
@@ -1,13 +1,33 @@
 import Head from "next/head"
 import Container from "../components/Container"
+import { getBlogposts } from "../utils/contentful-graphql"
 
-export default function BlogPosts() {
+
+
+export const getStaticProps = async () => {
+  const data = await getBlogposts()
+  return {
+    props: {
+      blogposts: data.blogpostCollection.items
+    }
+  }
+}
+
+export default function BlogPosts({blogposts}) {
   return (
     <Container>
       <Head>
         <title>Blogposts - Dainyl Cua</title>
       </Head>
-      <div>blog-posts</div>
+      <div>
+        {
+          blogposts.map((blogpost) => (
+            <div key={blogpost.slug}>
+              {blogpost.title}
+            </div>
+          ))
+        }
+      </div>
     </Container>
   )
 }
