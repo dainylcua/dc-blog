eac879f979d42272f1858e090b7e5d616ee9c1a8
dainylcua <dainylcua@gmail.com>
2022-01-07 18:50:39 -0600

    Podoro functional besides start time

--- components/Timer.js
+++ components/Timer.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 
 export default function Timer() {
-  const [startTime, setStartTime] = useState(100)
-  const [time, setTime] = useState(100)
+  const [productive, setProductive] = useState(true)
+  const [startTime, setStartTime] = useState(1000000)
+  const [time, setTime] = useState(1000000)
+  const [timerColor, setTimerColor] = useState('')
   const [timerStatus, setTimerStatus] = useState(false)
 
   useEffect(() => {
@@ -10,32 +12,58 @@ export default function Timer() {
 
     if(timerStatus) {
       interval = setInterval(() => {
-        setTime((prevTime) => {
-          prevTime--
-        })
+        setTime((prevTime) => prevTime-=1000)
       }, 1000)
+      setTimerColor('dark:bg-rose-500 bg-lime-500')
+    } else {
+      clearInterval(interval)
+      setTimerColor('dark:bg-sky-500 bg-amber-500')
     }
 
-  }, [timerStatus])
+    // Cleanup function (prevents memory leaks)
+    return () => clearInterval(interval)
+  }, [timerStatus, timerColor])
+
+  const toggleTimer = () => {
+    setTimerStatus(prevStatus => !prevStatus)
+  }
+
+  const resetTimer = () => {
+    setTime(startTime)
+    setTimerStatus(false)
+  }
+
+  const toggleProductive = () => {
+    setProductive(prevProductive => !prevProductive)
+    resetTimer()
+  }
 
 
   return(
     <section className="flex flex-col w-full">
       <div className="w-full h-1 bg-zinc-200 dark:bg-zinc-800">
-        <div className="dark:bg-rose-700 h-1" style={{width: `${time/startTime}%`}}></div>
+        <div className={`h-1 ${timerColor}`} style={{width: `${time/startTime}%`}}></div>
       </div>
       <div className="w-3/5 mx-auto flex flex-col items-center">
+        <div>
+          {productive ? 'Productive' : 'Break'} Time!
+        </div>
         <div>
           {time}
         </div>
         <div className="flex w-1/5 justify-center mx-auto gap-x-8">
-          <button onClick={() => setTimerStatus(prevStatus => !prevStatus)} className="h-8">
+          <button onClick={toggleTimer} className="h-8 font-medium">
             {
               timerStatus ? 'Stop' : 'Start'
             }
           </button>
-          <button className="h-8">
+          <button onClick={resetTimer} className="h-8 font-medium">
             Reset
+          </button>
+        </div>
+        <div className={timerStatus ? 'hidden' : 'block'}>
+          <button onClick={toggleProductive} className='h-8 font-medium'>
+              Switch to {productive ? 'Break' : 'Productive'}
           </button>
         </div>
       </div>
