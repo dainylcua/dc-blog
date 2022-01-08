f3523be22487c6f6e02175da82a45cf5129c721f
dainylcua <dainylcua@gmail.com>
2022-01-08 11:31:18 -0600

    Functional goals

--- pages/goals.js
+++ pages/goals.js
@@ -1,13 +1,51 @@
 import Head from "next/head"
+import { ReactMarkdown } from "react-markdown/lib/react-markdown"
 import Container from "../components/Container"
+import { getGoals } from "../utils/contentful-graphql"
 
-export default function Goals() {
+export const getStaticProps = async () => {
+  const data = await getGoals()
+  return {
+    props: {
+      goals: data.goalsCollection.items
+    }
+  }
+}
+
+export default function Goals({goals}) {
+  console.log(goals)
   return (
     <Container>
       <Head>
         <title>Goals - Dainyl Cua</title>
       </Head>
-      <div>goals</div>
+      <div className="flex flex-col">
+        <div className="flex">
+          <div>
+            Goal
+          </div>
+          <div>
+            Completion Date
+          </div>
+        </div>
+        {
+          goals.map((goal) => (
+          <div key={goal.goal} className="flex">
+            <div>
+              {goal.goal}
+            </div>
+            <div>
+              {
+                goal.completionStatus.completed ?
+                goal.completionStatus.completionDate
+                :
+                ''
+              }
+            </div>
+          </div>
+          ))
+        }
+      </div>
     </Container>
   )
 }
