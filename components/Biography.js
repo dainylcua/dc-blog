fc3dbe0941da6f19f65897b3a523e3e1e490d9e9
dainylcua <dainylcua@gmail.com>
2022-01-08 13:49:10 -0600

    Adds more styling to info

--- components/Biography.js
+++ components/Biography.js
@@ -2,9 +2,9 @@ export default function Biography() {
   return(
     <section className="flex flex-col w-full gap-y-4">
       <div className="text-5xl lg:text-7xl dark:text-rose-200">About Me</div>
-      <div className="text-">Hello again! I&apos;m Dainyl Cua, and I love developing elegant, accessible, and useful tools for many. I started my coding journey helping my sister with her HTML and CSS on her profile page for ToonTownCentral - a forum page for our one of our favorite games. After that, I took a summer course in middle school (and understood nothing) that covered Java - the only thing I remembered? System.out.println(). </div>
+      <div className="text-">Hello again! I&apos;m Dainyl Cua, and I love developing elegant, accessible, and useful tools for many. I started my coding journey helping my sister with her HTML and CSS on her profile page for ToonTownCentral - a forum page for our one of our favorite games. </div>
       <div>I graduated from Texas A&amp;M University in 2020 and learned to code in MATLAB for my numerical analysis class. Shortly after graduating, I started a consulting job which unfortunately fell through - but after that, I decided to pursue my true passion: coding.</div>
-      <div>In April 2021, I learned HTML, CSS, and JavaScript. After a month of honing my skills, I picked up the basics of Bootstrap CSS and React. I would continue to develop tiny projects here and there until September, where I joined General Assembly&apos;s Software Engineering Bootcamp and began developing 8-10 hours a day in multiple languages and tech stacks.</div>
+      <div>In April 2021 I started my software development journey and strengthened my fundamentals. In September, I joined General Assembly&apos;s Software Engineering Bootcamp and began developing 8-10 hours a day in multiple languages and tech stacks.</div>
     </section>
   )
 }
--- components/Skills.js
+++ components/Skills.js
@@ -3,19 +3,19 @@ export default function Skills() {
     <section className="flex flex-col w-full py-4 gap-y-4">
       <div>I&apos;m primarily a MERN stack and Jamstack developer, but I love picking up new technologies to expand my toolset. Check out my featured ones below!</div>
       <div className="flex flex-col items-start py-4 md:flex-row md:gap-y-4">
-        <ul className="flex flex-col flex-grow">
+        <ul className="flex flex-col w-1/3">
           <li>JavaScript</li>
           <li>React</li>
           <li>Next.js</li>
           <li>Tailwind CSS</li>
         </ul>
-        <ul className="flex flex-col flex-grow">
+        <ul className="flex flex-col w-1/3">
           <li>Node.js</li>
           <li>Express.js</li>
           <li>MongoDB</li>
           <li>Contentful</li>
         </ul>
-        <ul className="flex flex-col flex-grow">
+        <ul className="flex flex-col w-1/3">
           <li>Python</li>
           <li>Django</li>
           <li>PostgreSQL</li>
@@ -24,17 +24,17 @@ export default function Skills() {
       </div>
       <div>In addition, here are some technologies I&apos;m really dying to learn!</div>
       <div className="flex flex-col items-start py-4 md:flex-row md:gap-y-4">
-        <ul className="flex flex-col flex-grow">
+        <ul className="flex flex-col w-1/3">
           <li>TypeScript</li>
           <li>Jest</li>
           <li>Material UI</li>
         </ul>
-        <ul className="flex flex-col flex-grow">
+        <ul className="flex flex-col w-1/3">
           <li>Flask</li>
           <li>Rust</li>
           <li>Vue</li>
         </ul>
-        <ul className="flex flex-col flex-grow"></ul>
+        <ul className="flex flex-col w-1/3"></ul>
       </div>
     </section>
   )
