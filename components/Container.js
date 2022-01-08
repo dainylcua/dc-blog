a9d35bb69a785212399ee1c9d9a1c557e65e71e0
dainylcua <dainylcua@gmail.com>
2022-01-08 17:21:32 -0600

    Adds Dark Mode

--- components/Container.js
+++ components/Container.js
@@ -1,12 +1,14 @@
 import Footer from "./Footer/Footer"
 import Nav from "./Nav/Nav"
+import DarkToggle from "./DarkToggle"
 export default function Container({children}) {
   return (
     <div className="h-full min-h-screen font-normal leading-relaxed dark:bg-zinc-900 bg-zinc-100 dark:text-zinc-300">
       <div className="flex flex-col w-4/5 mx-auto md:w-[50rem]">
         <Nav />
-        <main className="py-12">
+        <main className="relative py-12">
           {children}
+        <DarkToggle />
         </main>
         <Footer />
       </div>
---
+++ components/DarkToggle.js
@@ -0,0 +1,46 @@
+import { MdDarkMode, MdLightMode } from "react-icons/md"
+import { IconContext } from "react-icons/lib"
+import { useEffect, useState } from "react"
+
+export default function DarkToggle() {
+  const [isDark, setIsDark] = useState(true)
+
+  useEffect(() => {
+    if(typeof window !== 'undefined') {
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+          setIsDark(true)
+          document.documentElement.classList.add('dark')
+        } else {
+          setIsDark(false)
+          document.documentElement.classList.remove('dark')
+        }
+    }
+  }, [])
+
+  useEffect(() => {
+    if(typeof window !== 'undefined') {
+      if(isDark) {
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark'
+      } else {
+        document.documentElement.classList.remove('dark')
+        localStorage.theme = 'light'
+      }
+    }
+  }, [isDark])
+
+  return(
+    <div className="absolute p-4 top-4 right-4">
+      <button onClick={() => setIsDark((prev) => !prev)}>
+        <IconContext.Provider value={{ className: "text-zinc-900 hover:text-zinc-500 dark:text-sky-200 dark:hover:text-sky-100 cursor-pointer ", size: "2.5rem"}}>
+          {
+            isDark ?
+              <MdDarkMode />
+            :
+              <MdLightMode />
+          }
+        </IconContext.Provider>
+      </button>
+    </div>
+  )
+}
