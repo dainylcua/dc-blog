4edf179ea4abe0996c85f6779bdf7bf9e31cc8a9
dainylcua <dainylcua@gmail.com>
2022-01-08 19:39:48 -0600

    Adds Pomodoro description

--- components/Container.js
+++ components/Container.js
@@ -3,7 +3,7 @@ import Nav from "./Nav/Nav"
 import DarkToggle from "./DarkToggle"
 export default function Container({children}) {
   return (
-    <div className="h-full min-h-screen font-normal leading-relaxed dark:bg-zinc-900 bg-zinc-100 dark:text-zinc-300">
+    <div className="h-full min-h-screen font-normal leading-relaxed transition-colors ease-in-out dark:bg-zinc-900 bg-zinc-100 dark:text-zinc-300">
       <div className="flex flex-col w-4/5 mx-auto md:w-[50rem]">
         <Nav />
         <main className="relative py-12">
--- pages/pomodoro.js
+++ pages/pomodoro.js
@@ -10,11 +10,20 @@ export default function Pomodoro() {
       <Head>
         <title>Pomodoro Timer - Dainyl Cua</title>
       </Head>
+      <Header>Pomodoro</Header>
+      <div>An barebones timer to help with the Pomodoro Technique. I use this a lot when I need to really focus on a task. Check out the Pomodro Technique&nbsp;
+        <a className="underline cursor-pointer text-sky-600 dark:text-sky-300"
+          target="_blank" 
+          rel="noreferrer noopener" 
+          href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+        >
+          here!
+        </a>
+      </div>
       <div>
         <Timer />
         <Todo />
       </div>
-      <Header>Pomodoro</Header>
     </Container>
   )
 }
